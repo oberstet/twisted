f3844ddbcd9877a4b7872744e22314bbc71b1bd5
Tobias Oberstein <tobias.oberstein@tavendo.de>
2013-11-18 20:07:01 +0100

    fix #6843

--- twisted/internet/test/_posixifaces.py
+++ twisted/internet/test/_posixifaces.py
@@ -30,7 +30,10 @@ if _PY3:
 
 libc = CDLL(find_library("c"))
 
-if sys.platform == 'darwin':
+if sys.platform.startswith('freebsd') or \
+   sys.platform.startswith('netbsd') or \
+   sys.platform.startswith('openbsd') or \
+   sys.platform == 'darwin':
     _sockaddrCommon = [
         ("sin_len", c_uint8),
         ("sin_family", c_uint8),
